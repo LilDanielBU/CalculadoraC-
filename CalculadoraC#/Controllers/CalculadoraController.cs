6de85135f20420a0b845562af3563e4397978ae8
CARLOS DANIEL MONCALEANO VARGAS <carlos.moncaleano@aviatur.com.co>
2026-07-03 16:35:56 -0500

    Fix:  Model Binding

--- CalculadoraC#/Controllers/CalculadoraController.cs
+++ CalculadoraC#/Controllers/CalculadoraController.cs
@@ -1,8 +1,41 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CalculadoraC_.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CalculadoraC_.Controllers
 {
-    public class CalculadoraControllerv : Controller
+    public class CalculadoraController : Controller
     {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var modelo = new CalculadoraViewModel();
+
+            return View(modelo);
+        }
+
+        [HttpPost]
+        public IActionResult Index(CalculadoraViewModel model)
+        {
+            switch (model.Operacion)
+            {
+                case "suma":
+                    model.Resultado = model.Numero1 + model.Numero2;
+                    break;
+                case "resta":
+                    model.Resultado = model.Numero1 - model.Numero2;
+                    break;
+                case "multiplicacion":
+                    model.Resultado = model.Numero1 * model.Numero2;
+                    break;
+                case "division":
+                    if (model.Numero2 != 0)
+                    {
+                        model.Resultado = model.Numero1 / model.Numero2;
+                    }
+                    break;
+            }
+
+            return View(model);
+        }
     }
 }
